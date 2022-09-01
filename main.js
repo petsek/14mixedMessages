842aaf974a4fccbe756639d4032c29e7edc9d382
petsek <peti.szekeres@gmail.com>
2022-09-01 22:45:17 +0100

    changes in "when"

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const who = ['The Bee', 'Mr Todd', 'Hedgehog Jr.', 'Dr Owl', 'Somebody']
 const what = ['jumped over the fence', 'stole an acorn', 'broke a window', 'eat all the pies', 'did nothing']
-const when = ['yesterday', '12/04/2021 at 5:37 pm', 'three days ago', 'on Monday', 'never']
+const when = ['yesterday', '12/04/2021 at 5:37 pm', 'three days ago', 'on Monday', '5 times a day']
 const where = ['at the rainbow', 'on the top of a tree', 'in the den', 'behind a bush', 'nowhere']
 const why = ['to show off', 'to kill some time', 'because he is a badass', 'to help out', 'for no reason']
 
